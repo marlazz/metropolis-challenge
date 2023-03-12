0b7d8162f68277249dd4a8b8bc992cba1a009139
Marla <zingermarla8@gmail.com>
2023-03-12 15:30:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import SHIRT from "./assets/1.jpeg";
+import COAT from "./assets/2.jpeg";
+import HOODIE from "./assets/3.jpeg";
+import { CgProfile } from "react-icons/cg";
+import { BsBag } from "react-icons/bs";
+import { BiMenu, BiX, BiHeart, BiSearch } from "react-icons/bi";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setisMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main-container">
+        <div className="top-bar">
+          <nav className="nav">
+            <span className="nav-item">shop</span>
+            <span className="nav-item">collection</span>
+            <span className="nav-item">about</span>
+            <span className="nav-item">contact</span>
+          </nav>
+          <div className="menu-burger">
+            {isMenuOpen ? (
+              <BiX onClick={handleMenuToggle} />
+            ) : (
+              <BiMenu onClick={handleMenuToggle} />
+            )}
+            {isMenuOpen && (
+              <ul className="menu-links">
+                <li className="menu-link">shop</li>
+                <li className="menu-link">collection</li>
+                <li className="menu-link">about</li>
+                <li className="menu-link">contact</li>
+              </ul>
+            )}
+          </div>
+          <div className="icons-container">
+            <BiSearch />
+            <CgProfile />
+            <BiHeart />
+            <BsBag />
+          </div>
+        </div>
+        <div className="logo">
+          <span className="logo__initials">c.p</span>
+          <span className="logo__text">compagny</span>
+        </div>
+        <h1 className="title">New Collection</h1>
+        <h2 className="subtitle">Metropolis series</h2>
+        <div className="gallery">
+          <div
+            className="product-image"
+            style={{ backgroundImage: `url(${SHIRT})` }}
+          />
+          <div
+            className="product-image"
+            style={{ backgroundImage: `url(${COAT})` }}
+          />
+          <div
+            className="product-image"
+            style={{ backgroundImage: `url(${HOODIE})` }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
